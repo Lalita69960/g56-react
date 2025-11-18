b73fd55bf6caebbe10fa73d3442687dd89dbd3ad
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-11-18 09:42:08 +0100

    Add LifecycleDemo component to demonstrate React lifecycle methods.

--- src/App.jsx
+++ src/App.jsx
@@ -3,51 +3,51 @@ import AlertMessage from "./2_lecture/AlertMessage";
 import PricingDynamic from "./2_lecture/PricingDynamic";
 import Pricing from "./2_lecture/Pricing";
 
-import { FaExclamationTriangle, FaInfoCircle } from "react-icons/fa";
+import {FaExclamationTriangle, FaInfoCircle} from "react-icons/fa";
 import Counter from "./2_lecture/Counter";
 import LikeButtons from "./2_lecture/LikeButtons";
 import PersonForm from "./2_lecture/PersonForm";
 
+import LifecycleDemo from "./3_lecture/LifecycleDemo.jsx";
+
 function App() {
-  return (
-    <>
-
-      <PersonForm/>
+    return (
+        <>
 
 
-      {/**
-        <Counter/>
-        <LikeButtons/>
-
-      */}
+            <LifecycleDemo/>
 
 
 
-      {/**
-       <Pricing/>
-      <PricingDynamic/>
-      */}
 
-      {/*
+            {/**
+             <PersonForm/>
 
-      <AlertMessage 
-      alertType="alert-success"
-      message="Operation was Successful!"/>
+             <Counter/>
+             <LikeButtons/>
 
-      
-      <AlertMessage 
-      alertType="alert-danger"
-      icon={<FaExclamationTriangle/>}
-      message="Operation has Failed"/>
+             <Pricing/>
+             <PricingDynamic/>
+             */}
 
-      <AlertMessage 
-      alertType="alert-info"
-      icon={<FaInfoCircle/>}
-      message="System is up and running "/>
-      
-      */}
-    </>
-  );
+            {/**
+             <AlertMessage
+             alertType="alert-success"
+             message="Operation was Successful!"/>
+
+
+             <AlertMessage
+             alertType="alert-danger"
+             icon={<FaExclamationTriangle/>}
+             message="Operation has Failed"/>
+
+             <AlertMessage
+             alertType="alert-info"
+             icon={<FaInfoCircle/>}
+             message="System is up and running "/>
+             */}
+        </>
+    );
 }
 
 export default App;
